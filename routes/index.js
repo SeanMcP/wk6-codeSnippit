55b90c68449040c4197c31ef609d3a78e23ad6e4
Sean McPherson <seanmcp25@gmail.com>
2017-08-26 16:33:55 -0400

    Add style to snippit page; simplify view structure

--- routes/index.js
+++ routes/index.js
@@ -47,8 +47,8 @@ router.post('/copy', function(req, res) {
   })
 })
 
-router.get('/create', function(req, res) {
-  res.render('create', {username: req.user.username})
+router.get('/create', requireLogin, function(req, res) {
+  res.render('snippit', {username: req.user.username, create:true})
 })
 
 router.post('/create', function(req, res) {
@@ -70,7 +70,7 @@ router.post('/create', function(req, res) {
   })
 })
 
-router.get('/delete/:id', function(req, res) {
+router.get('/delete/:id', requireLogin, function(req, res) {
   Snippit.findOne({_id: req.params.id}).remove().exec()
   res.redirect('/profile')
 })
@@ -94,9 +94,9 @@ router.post('/edit/:id', function(req, res) {
 })
 
 router.get('/language/:language', function(req, res) {
-  Snippit.find({'language': {$in: [req.params.language]}})
+  Snippit.find({'language': {$in: [req.params.language]}, 'public': true})
   .then(function(data) {
-    res.render('profile', {username: req.user.username, data: data})
+    res.render('public', {username: req.user.username, data: data})
   })
   .catch(function(err) {
     res.send(err)
@@ -121,21 +121,19 @@ router.get('/logout', function(req, res) {
 router.get('/profile', requireLogin, function(req, res) {
   Snippit.find({'author': req.user.username})
   .then(function(data) {
-    res.render('profile', {username: req.user.username, data: data})
+    res.render('user', {username: req.user.username, data: data, profile: true})
   })
   .catch(function(err) {
-    console.log('THERE IS AN ERROR!!!!!!\n', err);
-    res.redirect('/user')
+    res.send('err')
   })
 })
 
-router.get('/public', requireLogin, function(req, res) {
+router.get('/public', function(req, res) {
   Snippit.find({'public': true})
   .then(function(data) {
     res.render('public', {data: data})
   })
   .catch(function(err) {
-    console.log('THERE IS AN ERROR!!!!!!\n', err);
     res.send(err)
   })
 })
@@ -150,7 +148,6 @@ router.post('/signup', function(req, res) {
     password: req.body.password
   })
   .then(function(data) {
-    console.log(data)
     res.redirect('/')
   })
   .catch(function(err) {
@@ -160,37 +157,90 @@ router.post('/signup', function(req, res) {
 })
 
 router.get('/tag/:tag', function(req, res) {
-  Snippit.find({'tag': {$in: [req.params.tag]}})
+  Snippit.find({'tag': {$in: [req.params.tag]}, 'public': true})
   .then(function(data) {
-    res.render('profile', {username: req.user.username, data: data})
+    res.render('public', {username: req.user.username, data: data})
   })
   .catch(function(err) {
     res.send(err)
   })
 })
 
-router.get('/user/:author', requireLogin, function(req, res) {
-  Snippit.find({'author': req.params.author})
-  .then(function(data) {
-    if (req.params.author == req.user.username) {
-      res.render('profile', {username: req.params.author, data: data})
-    } else {
-      res.render('user', {username: req.params.author, data: data})
-    }
-  })
-  .catch(function(err) {
-    console.log('THERE IS AN ERROR!!!!!!\n', err);
-    res.send(err)
-  })
-})
-
-router.get('/view/:id', function(req, res) {
-    Snippit.find({'_id': req.params.id})
+router.get('/user/:author', function(req, res) {
+  if (!req.user) {
+    Snippit.find({'author': req.params.author, 'public': true})
     .then(function(data) {
-      if (data[0].author == req.user.username) {
-        res.render('edit', {data: data})
+      res.render('user', {username: req.params.author, data: data})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  } else if (req.params.author == req.user.username) {
+    Snippit.find({'author': req.params.author})
+    .then(function(data) {
+      res.render('user', {username: req.params.author, data: data, profile: true})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  } else {
+    Snippit.find({'author': req.params.author, 'public': true})
+    .then(function(data) {
+      res.render('user', {username: req.params.author, data: data})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  }
+})
+
+router.get('/user/:username/language/:language', function(req, res) {
+  if(req.params.username == req.user.username) {
+    Snippit.find({'language': {$in: [req.params.language]}, 'author': req.params.username})
+    .then(function(data) {
+        res.render('user', {username: req.params.username, data: data, profile: true})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  } else {
+    Snippit.find({'language': {$in: [req.params.language]}, 'author': req.params.username, 'public': true})
+    .then(function(data) {
+        res.render('user', {username: req.params.username, data: data})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  }
+})
+
+router.get('/user/:username/tag/:tag', function(req, res) {
+  if(req.params.username == req.user.username) {
+    Snippit.find({'tag': {$in: [req.params.tag]}, 'author': req.params.username})
+    .then(function(data) {
+        res.render('user', {username: req.params.username, data: data, profile: true})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  } else {
+    Snippit.find({'tag': {$in: [req.params.tag]}, 'author': req.params.username, 'public': true})
+    .then(function(data) {
+        res.render('user', {username: req.params.username, data: data})
+    })
+    .catch(function(err) {
+      res.send(err)
+    })
+  }
+})
+
+router.get('/view/:id', requireLogin, function(req, res) {
+    Snippit.findOne({'_id': req.params.id})
+    .then(function(data) {
+      if (data.author == req.user.username) {
+        res.render('snippit', {data: data, edit:true})
       } else {
-        res.render('view', {data: data})
+        res.render('snippit', {data: data, view: true})
       }
     })
     .catch(function(err) {
